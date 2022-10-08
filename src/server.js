7448da0a8a54d3f86688d6b3711f4ee379fa745e
d_flex <hiyoo95@gmail.com>
2022-10-08 19:24:51 +1000

    connect static files (pug template engine, js, and css)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('hello node server!');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use('/public', express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListen);
