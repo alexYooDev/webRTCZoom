1771ea9544333dbb72f49eac609cb4403f6a5c1e
d_flex <hiyoo95@gmail.com>
2022-10-19 18:42:59 +1000

    data transmission: plain text to json

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,12 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
-const $messageForm = document.querySelector('form');
+const $messageForm = document.querySelector('#message');
+const $nickNameForm = document.querySelector('#nickName');
 const $messageList = document.querySelector('ul');
 
 const handleSockectMessage = (message) => {
-  console.log(`New message: "${message.data}"`);
+  const li = document.createElement('li');
+  li.textContent = message.data;
+  $messageList.append(li);
 };
 
 socket.addEventListener('open', () => {
@@ -16,15 +19,25 @@ socket.addEventListener('close', () => {
   console.log('Disconnected from the server!');
 });
 
-const handleSubmit = (event) => {
+const handleMessageSubmit = (event) => {
   event.preventDefault();
   const $input = $messageForm.querySelector('input');
-  socket.send($input.value);
+  const data = JSON.stringify({ type: 'message', payload: $input.value });
+  socket.send(data);
   $input.value = '';
 };
 
-$messageForm.addEventListener('submit', handleSubmit);
+const handlenickNameSubmit = (event) => {
+  event.preventDefault();
+  const $input = $nickNameForm.querySelector('input');
+  const data = JSON.stringify({ type: 'nickname', payload: $input.value });
+  console.log(data);
+  socket.send(data);
+  $input.value = '';
+};
 
+$messageForm.addEventListener('submit', handleMessageSubmit);
+$nickNameForm.addEventListener('submit', handlenickNameSubmit);
 /* setTimeout(() => {
   socket.send('Hello from the browser!');
 }, 10000);
